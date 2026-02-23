b965e4d1b5a4501fe952d1a44728fd00d7decee1
terryhberg97 <terryhberg97@gmail.com>
2026-02-23 16:12:11 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class TheaterKiosk {
+    void main() {
+        Scanner in = new Scanner(System.in);
+
+        int age;
+        String Trash = "";
+
+        System.out.println("Enter your age: ");
+        age = in.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband");
+        }
+
+    }
+}
